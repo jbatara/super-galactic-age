efc71f71b1a7d17057bc43232824497d9c084f71
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-09-06 14:54:26 -0700

    Add Lifestyle class constructor

--- src/lifestyle.js
+++ src/lifestyle.js
@@ -1,5 +1,10 @@
 export class Lifestyle{
-  constructor(){
-    
+  constructor(gender, diet, smoking, activity, alcohol, bmi){
+    this.gender = gender;
+    this.diet = diet;
+    this.smoking = smoking;
+    this.activity = activity;
+    this.alcohol = alcohol;
+    this.bmi = bmi;
   }
 }
