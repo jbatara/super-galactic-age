4b9b0c359e9bbbdba3bb845aa8e14bff318c6877
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-09-06 14:40:33 -0700

    Add specs for Lifestyle
    -Add dad SuperGalacticAge obj.
    -Add dadLifestyle, dadLifestyle_poor, and dadLifestyle_nonUSA Lifestyle
    objs to use for testing alongside momLifestyle obj. Only difference is
    gender.
    -Test expectedLifeSpan method to return expected age based on gender for
    non US people
    -Test expectedLifeSpan method to return expected age based on lifestyle
    factors if person lives in US

--- spec/super-galactic-age-spec.js
+++ spec/super-galactic-age-spec.js
@@ -1,5 +1,9 @@
-import {SuperGalacticAge} from './../src/super-galactic-age';
-import {Lifestyle} from './../src/lifestyle';
+import {
+  SuperGalacticAge
+} from './../src/super-galactic-age';
+import {
+  Lifestyle
+} from './../src/lifestyle';
 
 describe('SuperGalacticAge', function () {
   beforeEach(function () {
@@ -69,11 +73,19 @@ describe('Lifestyle', function () {
     const newerBaby = new SuperGalacticAge(0);
     const baby = new SuperGalacticAge(1);
     const mom = new SuperGalacticAge(30);
+    const dad = new SuperGalacticAge(25);
     //lifestyle: gender, country, diet, smoking, physical activity, alcohol consumption, bmi
-    const momLifestyle = new Lifestyle('f', 'usa', '5d', '5s', '5p', '5a', '5b');
+    const momLifestyle = new Lifestyle('f', 'usa', 5, 1, 5, 2, 1);
+    const momLifestyle_poor = new Lifestyle('f', 'usa', 1, 5, 1, 5, 5);
+    const momLifestyle_nonUSA = new Lifestyle('f', 'china', 5, 1, 5, 2, 1);
     const momL_badEverythingA = new Lifestyle(2, '', 5035492346, -1, -12, 1.273, 12039);
-    const momL_badEverythingB = new Lifestyle('w', 'mars', 'ga','food','bar','foo','chicken');
+    const momL_badEverythingB = new Lifestyle('w', 'mars', 'ga', 'food', 'bar', 'foo', 'chicken');
     const momL_badEverythingC = new Lifestyle();
+
+    const dadLifestyle = new Lifestyle('m', 'usa', 5, 1, 5, 2, 1);
+    const dadLifestyle_poor = new Lifestyle('m', 'usa', 1, 5, 1, 5, 5);
+    const dadLifestyle_nonUSA = new Lifestyle('m', 'china', 5, 1, 5, 2, 1);
+
 
 
   });
@@ -92,20 +104,29 @@ describe('Lifestyle', function () {
     expect(momL_badGenderC.checkCountry()).toEqual(false);
   }
 
-   it('should return an error if an invalid entry was given for the user input') {
-     expect(momLifestyle.checkInput()).toEqual(true);
-     expect(momL_badGenderA.checkInput()).toEqual(false);
-     expect(momL_badGenderB.checkInput()).toEqual(false);
-     expect(momL_badGenderC.checkInput()).toEqual(false);
-   }
+  it('should return an error if an invalid entry was given for the user input') {
+    expect(momLifestyle.checkInput()).toEqual(true);
+    expect(momL_badGenderA.checkInput()).toEqual(false);
+    expect(momL_badGenderB.checkInput()).toEqual(false);
+    expect(momL_badGenderC.checkInput()).toEqual(false);
+  }
 
-   it('should return false when attempting to add a lifesytle that is invalid'){
+  it('should return false when attempting to add a lifesytle that is invalid') {
     expect(mom.addLifestyle(momLifestyle)).toEqual(true);
     expect(mom.addLifestyle(momL_badGenderA)).toEqual(false);
     expect(mom.addLifestyle(momL_badGenderB)).toEqual(false);
     expect(mom.addLifestyle(momL_badGenderC)).toEqual(false);
-   }
+  }
 
-  
+  it('should return the lifespan of those outside the USA with an average lifespan depending on gender') {
+    expect(momLifestyle_nonUSA.expectedLifeSpan()).toEqual(77.6);
+    expect(dadLifestyle_nonUSA.expectedLifeSpan()).toEqual(74.6);
+  }
 
+  it('should return the lifespan of those inside the USA depending on lifestyle factors') {
+    expect(momLifestyle.expectedLifeSpan()).toEqual(93.1);
+    expect(momLifestyle_poor.expectedLifeSpan()).toEqual(79);
+    expect(dadLifestyle.expectedLifeSpan()).toEqual(87.6);
+    expect(dadLifestyle_poor.expectedLifeSpan()).toEqual(75.5);
+  }
 });
