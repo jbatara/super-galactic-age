73bc3b371db2d1f96a7f726fca3dc8b2ad7a4c1c
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-09-07 00:11:13 -0700

    Clean variables, add expectedLifeSpan test case
    -Clean unused variables that were copy pasted into each section
    -Add Lifestyle objects with a non-us country, valid gender, but invalid
    lifestyle factor inputs. Show that the change in logic to tie being
    from the usa requires a checkInput does not effect returning life
    expectancy for non-usa Lifestyle objs

--- spec/lifespec.js
+++ spec/lifespec.js
@@ -31,6 +31,7 @@ describe('Lifestyle.checkGender', function () {
 describe('Lifestyle.checkCountry', function () {
 
   let momLifestyle;
+  let momLifestyle_nonUSA;
   let momL_badEverythingA;
   let momL_badEverythingB;
   let momL_badEverythingC;
@@ -38,13 +39,14 @@ describe('Lifestyle.checkCountry', function () {
   beforeEach(function () {
     //lifestyle: gender, country, diet, smoking, physical activity, alcohol consumption, bmi
     momLifestyle = new Lifestyle('f', 'usa', 5, 1, 5, 2, 1);
-
+    momLifestyle_nonUSA = new Lifestyle('f', 'china', 5, 1, 5, 2, 1);
     momL_badEverythingA = new Lifestyle(2, '', 5035492346, -1, -12, 1.273, 12039);
     momL_badEverythingB = new Lifestyle('w', 'mars', 'ga', 'food', 'bar', 'foo', 'chicken');
     momL_badEverythingC = new Lifestyle();
   });
   it('should return an error if an invalid entry was given for the user country', function () {
     expect(momLifestyle.checkCountry()).toEqual(true);
+    expect(momLifestyle_nonUSA.checkCountry()).toEqual(true);
     expect(momL_badEverythingA.checkCountry()).toEqual(false);
     expect(momL_badEverythingB.checkCountry()).toEqual(false);
     expect(momL_badEverythingC.checkCountry()).toEqual(false);
@@ -53,19 +55,13 @@ describe('Lifestyle.checkCountry', function () {
 
 describe('Lifestyle.checkInput', function () {
   let momLifestyle;
-
-  let momLifestyle_nonUSA;
   let momL_badEverythingA;
   let momL_badEverythingB;
   let momL_badEverythingC;
-  let dadLifestyle;
-  let dadLifestyle_poor;
-  let dadLifestyle_nonUSA;
   beforeEach(function () {
     //lifestyle: gender, country, diet, smoking, physical activity, alcohol consumption, bmi
     momLifestyle = new Lifestyle('f', 'usa', 5, 1, 5, 2, 1);
-    momLifestyle_nonUSA = new Lifestyle('f', 'china', 5, 1, 5, 2, 1);
-    momL_badEverythingA = new Lifestyle(2, '', 5035492346, -1, -12, 1.273, 12039);
+    momL_badEverythingA = new Lifestyle(2, '', 6, -1, -12, 1.273, 12039);
     momL_badEverythingB = new Lifestyle('w', 'mars', 'ga', 'food', 'bar', 'foo', 'chicken');
     momL_badEverythingC = new Lifestyle();
   });
@@ -82,30 +78,36 @@ describe('Lifestyle.expectedLifeSpan', function () {
   let momLifestyle;
   let momLifestyle_poor;
   let momLifestyle_nonUSA;
+  let momLifestyle_nonUSAi;
   let dadLifestyle;
   let dadLifestyle_poor;
   let dadLifestyle_nonUSA;
+  let dadLifestyle_nonUSAi;
   beforeEach(function () {
     //lifestyle: gender, country, diet, smoking, physical activity, alcohol consumption, bmi
     momLifestyle = new Lifestyle('f', 'usa', 5, 1, 5, 2, 1);
     momLifestyle_poor = new Lifestyle('f', 'usa', 1, 5, 1, 5, 5);
     momLifestyle_nonUSA = new Lifestyle('f', 'china', 5, 1, 5, 2, 1);
+    momLifestyle_nonUSAi = new Lifestyle('f', 'china');
     dadLifestyle = new Lifestyle('m', 'usa', 5, 1, 5, 2, 1);
     dadLifestyle_poor = new Lifestyle('m', 'usa', 1, 5, 1, 5, 5);
     dadLifestyle_nonUSA = new Lifestyle('m', 'china', 5, 1, 5, 2, 1);
+    dadLifestyle_nonUSAi = new Lifestyle('m', 'china');
 
 
 
   });
 
   it('should return the lifespan of those outside the USA with an average lifespan depending on gender', function () {
-    expect(momLifestyle_nonUSA.expectedLifeSpan()).toEqual(77.6);
-    expect(dadLifestyle_nonUSA.expectedLifeSpan()).toEqual(74.6);
+    expect(momLifestyle_nonUSA.expectedLifeSpan()).toEqual(77.02);
+    expect(momLifestyle_nonUSAi.expectedLifeSpan()).toEqual(77.02);
+    expect(dadLifestyle_nonUSA.expectedLifeSpan()).toEqual(73.97);
+    expect(dadLifestyle_nonUSAi.expectedLifeSpan()).toEqual(73.97);
   });
 
   it('should return the lifespan of those inside the USA depending on lifestyle factors', function () {
     expect(momLifestyle.expectedLifeSpan()).toEqual(93.1);
-    expect(momLifestyle_poor.expectedLifeSpan()).toEqual(79);
+    expect(momLifestyle_poor.expectedLifeSpan()).toEqual(79.0);
     expect(dadLifestyle.expectedLifeSpan()).toEqual(87.6);
     expect(dadLifestyle_poor.expectedLifeSpan()).toEqual(75.5);
   });
