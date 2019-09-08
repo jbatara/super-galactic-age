f01dd77496279a01fbb13dcf26e8375597498571
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-09-08 02:04:20 -0700

    Add SuperGalacticAge constructor and planet consts
    -Add constructor with if statment to create a SuperGalacticAge obj if
    the input is invalid
    -Constants for planetary ratios to earth's year were added for easy
    computation.

--- src/super-galactic-age.js
+++ src/super-galactic-age.js
@@ -1,7 +1,19 @@
-import {Lifestyle} from './lifestyle';
+import {
+  Lifestyle
+} from './lifestyle';
+const mercuryRatio = 0.24;
+const venusRatio = 0.62;
+const marsRatio = 1.88;
+const jupiterRatio = 11.86;
 
-export class SuperGalacticAge{
-  constructor(){
-    
+export class SuperGalacticAge {
+  constructor(age) {
+    if ((typeof age === 'number') && age >= 0) {
+      this.earth = age;
+      this.mercury = age/mercuryRatio;
+      this.venus = age / venusRatio;
+      this.mars = age / marsRatio;
+      this.jupiter = age / jupiterRatio;
+    }
   }
 }
